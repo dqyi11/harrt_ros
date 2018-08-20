18f0feb15696ce37965d1d32edb3f9e8af593d89
Daqing Yi <dqyi11@gmail.com>
2018-08-19 21:41:41 -0700

    clean for changes

--- include/harrt_ros/harrt_service.h
+++ include/harrt_ros/harrt_service.h
@@ -4,26 +4,18 @@
 #include <ros/ros.h>
 #include "harrt_ros/harrt_initialize.h"
 #include "harrt_ros/harrt_continue.h"
-#include "tpp/harrt/birrtstar.h"
 
 class HARRTService {
 public:
   HARRTService();
   virtual ~HARRTService();
 
-  birrts::BIRRTstar*           mp_harrt;
-  homotopy::ReferenceFrameSet* mp_reference_frame_set;
 
   bool get_paths( harrt_ros::harrt_initialize::Request& req,
                   harrt_ros::harrt_initialize::Response& res);
 
   bool refine_paths( harrt_ros::harrt_continue::Request& req,
                      harrt_ros::harrt_continue::Response& res);
-
-  void delete_harrt();
-
-  birrts::COST_FUNC_PTR func;
-  double**              fitness_distribution;
 
   ros::NodeHandle m_nh;
   ros::NodeHandle m_cont;
