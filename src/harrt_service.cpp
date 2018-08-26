956a959c6591868fb910be10eb5b9c9d3897b074
Daqing Yi <dqyi11@gmail.com>
2018-08-26 15:41:59 -0700

    update package.xml

--- src/harrt_service.cpp
+++ src/harrt_service.cpp
@@ -4,18 +4,23 @@
 #define HARRT_INIT_SERVICE_NAME "/harrt/get_paths"
 #define HARRT_CONT_SERVICE_NAME "/harrt/refine_paths"
 
-HARRTService::HARRTService() {
+HARRTService::HARRTService() 
+{
   m_harrt_init_srv = m_nh.advertiseService( HARRT_INIT_SERVICE_NAME, &HARRTService::get_paths, this);
   m_harrt_cont_srv = m_cont.advertiseService( HARRT_CONT_SERVICE_NAME, &HARRTService::refine_paths, this);
 }
 
-HARRTService::~HARRTService() {
+HARRTService::~HARRTService() 
+{
 
 }
 
 bool HARRTService::get_paths( harrt_ros::harrt_initialize::Request& req,
-                              harrt_ros::harrt_initialize::Response& res) {
+                              harrt_ros::harrt_initialize::Response& res) 
+{
+  // init service
 
+  // generate 
   return true;
 }
 
